f1f41a11e20be446f4e8b0faef513cf615a1abf9
miladsh02 <96131457+miladsh02@users.noreply.github.com>
2022-01-03 21:14:40 +0330

    Update 02.cpp
    I change 2 structs to 3 structs and I used Employee struct to link to WorkingInformation and PersonalInfo struct to use there variable and I think this is more clear than last version

--- 02.cpp
+++ 02.cpp
@@ -3,14 +3,16 @@
 #include<string>
 using namespace std;
 
-struct Employee 
+
+
+struct WorkingInformation
 {
 	int personalID;
 	double salary;
 	int workExperience;
-};//End of Employee struct
+};//End of WorkingInformation struct
 
-struct PersonalInfo 
+struct PersonalInfo
 {
 	string fullName;
 	string fatherName;
@@ -19,11 +21,17 @@ struct PersonalInfo
 	int numberOfchildren;
 };//End of PersonalInfo struct
 
+struct Employee
+{
+	struct WorkingInformation WI;
+	struct PersonalInfo PI;
+};//End of struct employee
+
+
 int main()
 {
-	const int numberOfEmployee = 40;
-	Employee employee[numberOfEmployee];
-	PersonalInfo employeePersonalInfo[numberOfEmployee];
+	const int numberOfEmployee = 4;
+	 Employee employee[numberOfEmployee];
 
 	for (int i = 0; i < numberOfEmployee; i++)
 	{
@@ -32,29 +40,29 @@ int main()
 
 		cout << "Please Enter Your Full Name: ";		
 		cin.ignore();
-		getline(cin , employeePersonalInfo[i].fullName);
+		getline(cin , employee[i].PI.fullName);
 
 		cout << "Please Enter Your father Name:";
 		cin.ignore();
-		getline(cin, employeePersonalInfo[i].fatherName);
+		getline(cin, employee[i].PI.fatherName);
 
 		cout << "What is your current marriage situation (if you are single \nright now you can enter 0 if you are not single enter 1):";
-		cin >> employeePersonalInfo[i].marriageSituation;
+		cin >> employee[i].PI.marriageSituation;
 		
 		cout << "How many children do you have:";
-		cin >> employeePersonalInfo[i].numberOfchildren;
+		cin >> employee[i].PI.numberOfchildren;
 
 		cout << "Please Enter Your Personal ID:";
-		cin >> employee[i].personalID;
+		cin >> employee[i].WI.personalID;
 
 		cout << "Please Enter Your ID:";
-		cin >> employeePersonalInfo[i].iD;
+		cin >> employee[i].PI.iD;
 
 		cout << "Please Enter Your salary:";
-		cin >> employee[i].salary;
+		cin >> employee[i].WI.salary;
 
 		cout << "Please enter how many years of experience do you have :";
-		cin >> employee[i].workExperience;
+		cin >> employee[i].WI.workExperience;
 
 		cout << endl << endl;
 
@@ -67,7 +75,7 @@ int main()
 	double sortedsalary[numberOfEmployee];
 	for (int i = 0; i < numberOfEmployee; i++)
 	{
-		sortedsalary[i] = employee[i].salary;
+		sortedsalary[i] = employee[i].WI.salary;
 	}//End of for
 
 	for (int i = 0; i < numberOfEmployee; i++)
@@ -90,7 +98,7 @@ int main()
 	{
 		for (int j = 0; j < numberOfEmployee; j++)
 		{
-			if (sortedsalary[i] == employee[j].salary)
+			if (sortedsalary[i] == employee[j].WI.salary)
 			{
 				max[z] = j;
 				z++;
@@ -105,16 +113,16 @@ int main()
 	cout << "Sorted by salary(low to high):" << endl << endl << endl;
 	for (int i = 0; i < numberOfEmployee; i++)
 	{
-		cout <<"Full name: " << employeePersonalInfo[max[i]].fullName << endl;
-		cout <<"Salary: " << employee[max[i]].salary << " toman" << endl;
-		cout <<"Father name: " << employeePersonalInfo[max[i]].fatherName<< endl;
-		if(employeePersonalInfo[max[i]].marriageSituation==1)
+		cout <<"Full name: " << employee[max[i]].PI.fullName << endl;
+		cout <<"Salary: " << employee[max[i]].WI.salary << " toman" << endl;
+		cout <<"Father name: " << employee[max[i]].PI.fatherName<< endl;
+		if(employee[max[i]].PI.marriageSituation==1)
 		cout << "MarriageSituation: " << " Married" << endl;
 		else
 		cout <<"MarriageSituation: " << "Single" << endl;
-		cout <<"Personal ID: " << employee[max[i]].personalID << endl;
-		cout <<"ID: " << employeePersonalInfo[max[i]].iD<< endl;
-		cout <<"Years of work experience: " << employee[max[i]].workExperience << " years" << endl;
+		cout <<"Personal ID: " << employee[max[i]].WI.personalID << endl;
+		cout <<"ID: " << employee[max[i]].PI.iD<< endl;
+		cout <<"Years of work experience: " << employee[max[i]].WI.workExperience << " years" << endl;
 		cout << endl << endl;
 	}//End of for
 	cout << "*************************************************************" << endl;
@@ -126,18 +134,18 @@ int main()
 	cout << "Filter ,Just show  who have 2 kids and less than  4 milion sallary" << endl << endl << endl;
 	for (int i = 0; i < numberOfEmployee; i++)
 	{
-		if (employeePersonalInfo[i].numberOfchildren == 2 && employee[i].salary < 4000000)
+		if (employee[i].PI.numberOfchildren == 2 && employee[i].WI.salary < 4000000)
 		{
-			cout << "Full name: " << employeePersonalInfo[i].fullName << endl;
-			cout << "Salary: " << employee[i].salary << " toman" << endl;
-			cout << "Father name: " << employeePersonalInfo[i].fatherName << endl;
-			if (employeePersonalInfo[max[i]].marriageSituation == 1)
+			cout << "Full name: " << employee[i].PI.fullName << endl;
+			cout << "Salary: " << employee[i].WI.salary << " toman" << endl;
+			cout << "Father name: " << employee[i].PI.fatherName << endl;
+			if (employee[max[i]].PI.marriageSituation == 1)
 				cout << "MarriageSituation: " << " Married" << endl;
 			else
 				cout << "MarriageSituation: " << "Single" << endl;
-			cout << "Personal ID: " << employee[i].personalID << endl;
-			cout << "ID: " << employeePersonalInfo[i].iD << endl;
-			cout << "Years of work experience: " << employee[i].workExperience << " years" << endl;
+			cout << "Personal ID: " << employee[i].WI.personalID << endl;
+			cout << "ID: " << employee[i].PI.iD << endl;
+			cout << "Years of work experience: " << employee[i].WI.workExperience << " years" << endl;
 			cout << endl << endl;		
 		}//End of if
 	}//End of for
@@ -152,18 +160,18 @@ int main()
 	cout << "Filter , Just show who are single and have more than 5 years work exprience" << endl << endl << endl;
 	for (int i = 0; i < numberOfEmployee; i++)
 	{
-		if (employeePersonalInfo[i].marriageSituation == 0 && employee[i].workExperience>5)
+		if (employee[i].PI.marriageSituation == 0 && employee[i].WI.workExperience>5)
 		{
-			cout << "Full name: " << employeePersonalInfo[i].fullName << endl;
-			cout << "Salary: " << employee[i].salary << " toman" << endl;
-			cout << "Father name: " << employeePersonalInfo[i].fatherName << endl;
-			if (employeePersonalInfo[i].marriageSituation == 1)
+			cout << "Full name: " << employee[i].PI.fullName << endl;
+			cout << "Salary: " << employee[i].WI.salary << " toman" << endl;
+			cout << "Father name: " << employee[i].PI.fatherName << endl;
+			if (employee[i].PI.marriageSituation == 1)
 				cout << "MarriageSituation: " << " Married" << endl;
 			else
 				cout << "MarriageSituation: " << "Single" << endl;
-			cout << "Personal ID: " << employee[i].personalID << endl;
-			cout << "ID: " << employeePersonalInfo[i].iD << endl;
-			cout << "Years of work experience: " << employee[i].workExperience << " years" << endl;
+			cout << "Personal ID: " << employee[i].WI.personalID << endl;
+			cout << "ID: " << employee[i].PI.iD << endl;
+			cout << "Years of work experience: " << employee[i].WI.workExperience << " years" << endl;
 			cout << endl << endl;
 		}//End of if
 	}//End of for
